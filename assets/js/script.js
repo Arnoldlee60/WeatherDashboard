ed24f010e6239da9c0fcb683077469960e7d63ef
Arnold Lee <arnoldlee60@gmail.com>
2021-06-04 18:42:40 -0500

    Trying to make the input work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,9 @@
 var urlWeather = 'https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=64d2e1dc27161dd62df3dc524d4db2bd'
 //http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&dt=1586468027&appid={API key}
-/*
-fetch(urlWeather)
-  .then(function (data) {
-    console.log('Fetch Response \n-------------');
-    console.log(data);
-  });
-  */
+var inputedCity = document.querySelector('input');
 
-  fetch('https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=64d2e1dc27161dd62df3dc524d4db2bd', {
+fetch('https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=64d2e1dc27161dd62df3dc524d4db2bd', 
+{
   method: 'GET', //GET is the default.
   credentials: 'same-origin', // include, *same-origin, omit
   redirect: 'follow', // manual, *follow, error
@@ -20,3 +15,7 @@ fetch(urlWeather)
     console.log(data);
     console.log(data.city.name);
   });
+
+  function search(){
+    console.log("City is = " + inputedCity)
+  }
