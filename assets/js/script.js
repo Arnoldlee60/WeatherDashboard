71786cfc21bdd4fa53513ed07ed333b029935af5
Arnold Lee <arnoldlee60@gmail.com>
2021-06-03 23:25:43 -0500

    api is working

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var urlWeather = 'https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=64d2e1dc27161dd62df3dc524d4db2bd'
+//http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&dt=1586468027&appid={API key}
+fetch(urlWeather)
+  .then(function (data) {
+    console.log('Fetch Response \n-------------');
+    console.log(data);
+  });
