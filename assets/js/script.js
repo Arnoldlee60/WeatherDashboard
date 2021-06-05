fae5d8006f4db579600061ed89e69b26c0d92367
Arnold Lee <arnoldlee60@gmail.com>
2021-06-04 21:40:25 -0500

    got the place and date working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
   function search(){
     var inputedCity = document.getElementById("cityName").value;
     console.log("City is = " + inputedCity)
-    
-fetch('https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=64d2e1dc27161dd62df3dc524d4db2bd', 
+    var newUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + inputedCity + '&appid=64d2e1dc27161dd62df3dc524d4db2bd';
+fetch(newUrl, 
 {
   method: 'GET', //GET is the default.
   credentials: 'same-origin', // include, *same-origin, omit
@@ -14,5 +14,16 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=64d2e1dc
   })
   .then(function (data) {
     console.log(data);
+    var today = new Date();
+    var date = ' (' + (today.getMonth()+1) + '-' + today.getDate() + '-' + today.getFullYear() + ')';
+    var x = document.createElement("HEADER");
+    x.setAttribute("id", "myHeader");
+    document.body.appendChild(x);
+    var y = document.createElement("H3"); 
+    var t = document.createTextNode(data.name + date);
+    y.appendChild(t);
+    document.getElementById("textarea").appendChild(y);
   });
   }
+
+
